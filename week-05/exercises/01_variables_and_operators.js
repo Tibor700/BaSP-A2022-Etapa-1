af823211eb7ab0193f84bdeae63eb85a01dbae39
Tibor takacs <111072050+Tibor700@users.noreply.github.com>
2022-09-19 02:33:31 +0200

    BaSP-W05-2022: Add third exercise of array.

--- week-05/exercises/01_variables_and_operators.js
+++ week-05/exercises/01_variables_and_operators.js
@@ -1,13 +1,18 @@
+console.log('exercise 1 variables and operators')
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+console.log('Exercise 1.a:')
 var numero1 = 3;
 var numero2 = 6;
 var suma = numero1 + numero2;
 console.log('la suma es:',suma);
-
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+console.log('Exercise 1.b:')
 var nombre = 'pepito';
 var apellido = 'perez';
 var nombrecompleto = nombre + apellido;
 console.log("El nombre es",nombrecompleto);
-
+// Crear dos variables de tipo String y sumar el largo de cada variable guardando el resultado de la suma en una 3er variable
+console.log('Exercise 1.c:')
 var pais = 'uruguay';
 var ciudad = 'montevideo';
 var sumalargo = pais.length + ciudad.length;
---
+++ week-05/exercises/03_arrays.js
@@ -0,0 +1,38 @@
+console.log('Exercise 3 array')
+// Dado el array mostrar por consola los meses 5 y 11
+console.log('Exercise 3.a:')
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log('los meses 5 y 11 son :',meses[5-1],meses[11-1]);
+
+// Ordenar el array de meses alfabéticamente y mostrarlo por consola
+console.log('Exercise 3.b:')
+console.log('el arreglo ordenado alfabeticamente:',meses.sort());
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+// Agregar un elemento al principio y al final del array
+console.log('Exercise 3.c:')
+meses.push('ultimo');
+meses.unshift('primero');
+console.log('estos son los elementos agregados:',meses);
+
+// Quitar un elemento del principio y del final del array
+console.log('Exercise 3.d:')
+meses.pop();
+meses.shift();
+console.log('arreglo con los elementos eliminados:',meses);
+
+// Invertir el orden del array
+console.log('Exercise 3.e:')
+meses.reverse();
+console.log('este es el arreglo invertido:',meses);
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+// Unir todos los elementos del array en un único string donde cada mes este separado por un guión
+console.log('Exercise 3.f:')
+var strmeses = meses.join('-');
+console.log('este es el string con guiones:',strmeses);
+
+// Crear una copia del array de meses que contenga desde Mayo hasta Noviembre
+console.log('Exercise 3.g:')
+var mesesalterno =meses.slice(4,11);
+console.log('este es el array alterno:',mesesalterno);
