3c504bf41976251f573aa23d0dae3384fbeebb3f
Tibor takacs <111072050+Tibor700@users.noreply.github.com>
2022-09-19 05:24:15 +0200

    BaSP-W05-2022: Add fifth exercise of for.

--- week-05/exercises/02_string.js
+++ week-05/exercises/02_string.js
@@ -1,30 +1,30 @@
-console.log('Exercise 2 string')
+console.log('Exercise 2 string');
 // Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula
-console.log('Exercise 2.a:')
+console.log('Exercise 2.a:');
 var nombreciudad = 'montevideo'
 console.log('El nombre en mayuscula es:',nombreciudad.toUpperCase());
 
 // Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres
 // guardando el resultado en una nueva variable
-console.log('Exercise 2.b:')
+console.log('Exercise 2.b:');
 var substr = nombreciudad.substring(0,5);
 console.log('el substring es:',substr);
 
 // Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 caracteres
 // guardando el resultado en una nueva variable
-console.log('Exercise 2.c:')
+console.log('Exercise 2.c:');
 var substr2 = nombreciudad.substring(nombreciudad.length-3,nombreciudad.length);
 console.log('el substring2 es:',substr2);
 
 // Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula
 // y las demás en minúscula.Guardar el resultado en una nueva variable
-console.log('Exercise 2.d:')
+console.log('Exercise 2.d:');
 var substr3 = nombreciudad.toUpperCase().substring(0,1) + nombreciudad.toLowerCase().substring(1,nombreciudad.length);
 console.log('el substring3 es :',substr3);
 
 // Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco.
 // Encontrar la posición del primer espacio en blanco y guardarla en una variable
-console.log('Exercise 2.e:')
+console.log('Exercise 2.e:');
 var strblanc ='pedro picapiedra';
 var blancposition = strblanc.indexOf(' ');
 console.log('esta es la posicion del espacio:',blancposition);
@@ -32,7 +32,7 @@ console.log('esta es la posicion del espacio:',blancposition);
 // Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
 // Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de
 // ambas palabras en mayúscula y las demás letras en minúscula
-console.log('Exercise 2.f:')
+console.log('Exercise 2.f:');
 var ciudadpais = 'montevideo uruguay';
 var substr4 = ciudadpais.toUpperCase().substring(0,1) + ciudadpais.toLowerCase().substring(1,ciudadpais.indexOf(' ')+1) +
 ciudadpais.toUpperCase().substring(ciudadpais.indexOf(' ')+1,ciudadpais.indexOf(' ')+2) +
---
+++ week-05/exercises/05_for.js
@@ -0,0 +1,39 @@
+console.log('Exercise 5 for');
+// Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript
+// para mostrar una alerta utilizando cada una de las palabras.
+console.log('Exercise 5.a:');
+var semana = ['lunes','martes','miercoles','jueves','viernes'];
+for (i=0; i<semana.length; i++) {
+alert(semana[i]);
+}
+console.log(semana);
+
+// Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
+console.log('Exercise 5.b:');
+for (i=0; i<semana.length; i++) {
+semana[i] = semana[i].toUpperCase().substring(0,1) + semana[i].toLowerCase().substring(1,semana[i].length);
+alert(semana[i]);
+}
+console.log(semana);
+
+// Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto (a)
+// recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence.
+// Al final mostrar una única alerta con la cadena completa.
+console.log('Exercise 5.c:');
+var sentence ='';
+semana = ['lunes','martes','miercoles','jueves','viernes'];
+for (i=0; i<semana.length; i++) {
+sentence =  sentence + semana [i];
+}
+alert(sentence);
+console.log(sentence);
+
+// Crear una array vacío y con un bucle for de 10 repeticiones. Llenar con el número de la repetición,
+// al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9.
+// Mostrar por la consola del navegador el array final
+console.log('Exercise 5.d:');
+var arraycontainer = [];
+for (i=0; i<10; i++) {
+arraycontainer.push(i);
+}
+console.log('este es el array lleno:',arraycontainer);
